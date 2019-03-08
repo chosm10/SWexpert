e94b6a6b2d60e3338ee9d552459754f64594be91
PEOPLECOM <PEOPLECOM@DESKTOP-QANRI3O>
2019-03-08 20:25:05 +0900

    홈 방범 서비스 완료

--- SWexpert/src/Solution2117.java
+++ SWexpert/src/Solution2117.java
@@ -10,10 +10,10 @@ public class Solution2117 {
 	static int N, M, K;
 	static int[][] board;
 	static Queue<Point> q = new LinkedList<>();
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
 	static int maxHome;
-	
+
 	static class Point {
 		int x, y;
 
@@ -21,82 +21,91 @@ public class Solution2117 {
 			this.x = x;
 			this.y = y;
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine().trim());
-		for(int tc = 1; tc <= T; tc++) {
+		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			board = new int[N][N];
-			
+
 			int home = 0;
-			for(int i = 0; i < N; i++) {
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < N; j++) {
+				for (int j = 0; j < N; j++) {
 					board[i][j] = Integer.parseInt(st.nextToken());
-					if(board[i][j] == 1) {
+					if (board[i][j] == 1) {
 						home++;
 					}
 				}
 			}
 			maxHome = 0;
-			
+			K = getK(home * M);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					bfs(j, i);
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(maxHome).append("\n");
 		}
+		System.out.println(sb);
 	}
-	
+
 	static void bfs(int x, int y) {
 		boolean[][] visited = new boolean[N][N];
 		visited[y][x] = true;
 		q.offer(new Point(x, y));
-		
+
 		int cnt = 0;
 		int home = 0;
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			int qsize = q.size();
-			//cnt가 최대 K가 되면 중지
-			if(cnt == K) {
+			// cnt가 최대 K가 되면 중지
+			if (cnt == K) {
 				q.clear();
 				break;
 			}
-			for(int i = 0; i < qsize; i++) {
+
+			for (int i = 0; i < qsize; i++) {
 				Point p = q.poll();
-				if(board[p.y][p.x] == 1) {
+				if (board[p.y][p.x] == 1) {
 					home++;
 				}
-				for(int j = 0; j < 4; j++) {
+				for (int j = 0; j < 4; j++) {
 					int nx = p.x + dx[j];
 					int ny = p.y + dy[j];
-					if(nx < 0 || nx >= N || ny < 0 || ny >= N)
+					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
 						continue;
-					
-					if(!visited[ny][nx]) {
+
+					if (!visited[ny][nx]) {
 						visited[ny][nx] = true;
 						q.offer(new Point(nx, ny));
 					}
 				}
 			}
-			if(home * M > getPay(cnt)) {
-				
-			}
 			cnt++;
+			if (home * M >= getPay(cnt)) {
+				if (maxHome < home)
+					maxHome = home;
+			}
 		}
 	}
-	
+
 	static int getK(int num) {
 		int i = 1;
-		while(true) {
+		while (true) {
 			int pay = getPay(i);
-			if(pay > num)
+			if (pay > num)
 				return i - 1;
 			i++;
 		}
 	}
-	
+
 	static int getPay(int k) {
-		return (int)(Math.pow(k, 2) + Math.pow(k - 1, 2));
+		return (int) (Math.pow(k, 2) + Math.pow(k - 1, 2));
 	}
 }
